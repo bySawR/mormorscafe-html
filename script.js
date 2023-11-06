7b1cef0f9152865cbb307c46317bc954394ff311
bySawR <marcusaevensen@gmail.com>
2023-11-06 15:47:50 +0100

    Update

--- script.js
+++ script.js
@@ -3,9 +3,13 @@ const sideMenu = document.getElementById("side-menu");
 const closeMenuBtn = document.getElementById("close-menu");
 
 menuIcon.addEventListener("click", () => {
-    sideMenu.style.right = "0"; /* Updated to slide in from the right */
+    sideMenu.style.right = "0"; // Slide in from the right
+    sideMenu.style.visibility = "visible"; // Set the visibility to visible when opening
 });
 
 closeMenuBtn.addEventListener("click", () => {
-    sideMenu.style.right = "-300px"; /* Updated to slide out to the right */
+    sideMenu.style.right = "-400px"; // Slide out to the right
+    setTimeout(() => {
+        sideMenu.style.visibility = "hidden"; // Set the visibility to hidden after the transition
+    }, 300); // Match the transition time (0.3s)
 });
